d9d7985f5599deb17b904f93f4a30fa42636dd3d
Werner Süß <werner.suess@itsv.at>
2024-01-19 12:45:15 +0100

    UTF8 icon added

--- get-latest-alerts.sh
+++ get-latest-alerts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
+echo "ZAMG: aktuell registrierte Erdbeben"
 while true; do
     wget -q https://www.zamg.ac.at/cms/de/geophysik/erdbeben/aktuelle-erdbeben/karten-und-listen || {
         echo "WGET don ZAMG nicht erfolgreich."
         exit 2
         }
     year=$(date +%Y)
-    echo -e "⛳\c"
-    grep location karten-und-listen | grep ${year} | head -n 1 | awk -F'strong>' '{print $2 ", " $4 "; " $9 "; " $12}' | sed 's/<\///g'
+    echo -e "🌍 \c"
+    grep location karten-und-listen | grep ${year} | head -n 1 | awk -F'strong>' '{print $2 ", " $4 ": " $9 "; " $12}' | sed 's/<\///g'
     rm karten-und-listen*
     sleep 3600
 done
