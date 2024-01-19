8a5149eac4ce57c34ef17a8feddbd2599e6bffef
Werner Süß <werner.suess@itsv.at>
2024-01-19 12:41:58 +0100

    while-oolp

--- get-latest-alerts.sh
+++ get-latest-alerts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-wget -q https://www.zamg.ac.at/cms/de/geophysik/erdbeben/aktuelle-erdbeben/karten-und-listen || {
-    echo "WGET don ZAMG nicht erfolgreich."
-    exit 2
-    }
-year=$(date +%Y)
-grep location karten-und-listen | grep ${year} | head -n 1 | awk -F'strong>' '{print $2 "; " $4 "; " $9 "; " $12}' | sed 's/<\///g'
-rm karten-und-listen*
-exit 0
+while true; do
+    wget -q https://www.zamg.ac.at/cms/de/geophysik/erdbeben/aktuelle-erdbeben/karten-und-listen || {
+        echo "WGET don ZAMG nicht erfolgreich."
+        exit 2
+        }
+    year=$(date +%Y)
+    echo -e "⛳\c"
+    grep location karten-und-listen | grep ${year} | head -n 1 | awk -F'strong>' '{print $2 ", " $4 "; " $9 "; " $12}' | sed 's/<\///g'
+    rm karten-und-listen*
+    sleep 3600
+done
